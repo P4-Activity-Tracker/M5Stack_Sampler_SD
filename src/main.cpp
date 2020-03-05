07c05470e85d2e1e9bc64386e725a63085cf02bf
nikohecht1 <61183172+nikohecht1@users.noreply.github.com>
2020-03-05 11:43:40 +0100

    noice

--- src/main.cpp
+++ src/main.cpp
@@ -161,19 +161,19 @@ void processDoSample() {
 	portENTER_CRITICAL(&mux);
 	doSample = false;
 	portEXIT_CRITICAL(&mux);
-	int16_t messurements[6];
+	float messurements[6];
 	//Sample accelerometer - Save to local variable
 	IMU.readAccelData(IMU.accelCount); //Læser x,y,z ADC værdierne
-	//IMU.getAres(); // Get accelerometer skale saved to "Ares"
-	messurements[0] = IMU.accelCount[0];//*IMU.aRes; //accelbias [0]
-	messurements[1] = IMU.accelCount[1];//*IMU.aRes; //accelbias [1]
-	messurements[2] = IMU.accelCount[2];//*IMU.aRes; //accelbias [2]
+	IMU.getAres(); // Get accelerometer skale saved to "Ares"
+	messurements[0] = (float)IMU.accelCount[0]*IMU.aRes; //accelbias [0]
+	messurements[1] = (float)IMU.accelCount[1]*IMU.aRes; //accelbias [1]
+	messurements[2] = (float)IMU.accelCount[2]*IMU.aRes; //accelbias [2]
 	// Sample gyroscope - Save to local variable
 	IMU.readGyroData(IMU.gyroCount);
-	//IMU.getGres(); // Get gyroskope skale saved to "Gres"
-	messurements[3] = IMU.gyroCount[0];//*IMU.gRes; //gyrobias [0]
-	messurements[4] = IMU.gyroCount[1];//*IMU.gRes; //gyrobias [1]
-	messurements[5] = IMU.gyroCount[2];//*IMU.gRes; //gyrobias [2]
+	IMU.getGres(); // Get gyroskope skale saved to "Gres"
+	messurements[3] = (float)IMU.gyroCount[0]*IMU.gRes; //gyrobias [0]
+	messurements[4] = (float)IMU.gyroCount[1]*IMU.gRes; //gyrobias [1]
+	messurements[5] = (float)IMU.gyroCount[2]*IMU.gRes; //gyrobias [2]
 	// Write to datafile
 	String dataString = "";
 	for (uint8_t i = 0; i < 6; i++) {
